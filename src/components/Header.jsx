92553813789b7c0f1c0df18fcd0b008a920c4cb6
trufflie <truffliegg@gmail.com>
2024-03-27 00:35:30 -0700

    initial commit for production

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+function Header() {
+  return (
+   
+   <header>
+      <h1><HighlightIcon />Trufflie, Web Developer</h1>
+    </header>
+   
+  );
+}
+
+export default Header;
